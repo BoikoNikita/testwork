690fbf272daff2e7de40ca0b6b585de4ba2d548b
«Nikita_Boiko» <nikita-bojko@yandex.ru>
2022-06-18 14:09:31 +0300

    task1 new array

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,1 +1,9 @@
 ﻿string[]array = {"2022", "tea", "=)","a+b"};
+
+int newArrayLength = 0;
+for (int i = 0; i < 4; i++)
+{
+    if (array[i].Length <= 3) 
+    newArrayLength++;
+}
+string[] newArray = new string[newArrayLength];
