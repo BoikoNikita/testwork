cb394f586c3069128b6042b27bc903f27b752270
«Nikita_Boiko» <nikita-bojko@yandex.ru>
2022-06-18 14:25:14 +0300

    result v1.1

--- task1/Program.cs
+++ task1/Program.cs
@@ -3,7 +3,7 @@
 int newArrayLength = 0;
 for (int i = 0; i < 4; i++)
 {
-    if (array[i].Length <= 3) 
+    if (array[i].Length < 4) 
     newArrayLength++;
 }
 string[] newArray = new string[newArrayLength];
@@ -11,7 +11,7 @@ string[] newArray = new string[newArrayLength];
 int count = 0; 
 for (int k = 0; k < 4; k++)
 {
-    if (array[k].Length <= 3)
+    if (array[k].Length < 4)
     {
         newArray[count] = array[k];
         count++;
